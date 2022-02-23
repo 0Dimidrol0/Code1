42075620e740e878abeb5643c4b2c9b59a65e2af
0Dimidrol0 <91985906+0Dimidrol0@users.noreply.github.com>
2022-02-23 14:58:51 +0400

    Add files via upload

---
+++ Task 2.kt
@@ -0,0 +1,48 @@
+fun main(){
+    val embassysEmployeesName: String = "Adam"
+    val emigrant: String = "Emigrant"
+
+    var embassysEmployeesText: String = ": Hi, sir. My name is Adam, how can i help you.\n"
+    var emigrantText: String = ": Hi, sir. I want to leave my country and move to your country.\n"
+
+        print("Conversation between emigrant and embassy`s employee.\n\n"+embassysEmployeesName+embassysEmployeesText+emigrant+emigrantText)
+
+    embassysEmployeesText = ": What`s the reason, sir?\n"
+    emigrantText = ": It seems war will soon start.\n"
+
+        print(embassysEmployeesName+embassysEmployeesText+emigrant+emigrantText)
+
+    embassysEmployeesText = ": Yes, that`s strong argument. but don`t you wanna protect your country, aren`t you patriot?\n"
+    emigrantText = ": Yes, i am. But i`m afraid of my wife health.\n"
+
+        print(embassysEmployeesName+embassysEmployeesText+emigrant+emigrantText)
+
+    embassysEmployeesText = ": What about children, sir?\n"
+    emigrantText = ": I haven`t children yet.\n"
+
+        print(embassysEmployeesName+embassysEmployeesText+emigrant+emigrantText)
+
+    embassysEmployeesText = ": That`s not meet for emigration requirements.\n"
+    emigrantText = ": But i`m an accountant with 10 years experience and i can be useful for your country.\n"
+
+        print(embassysEmployeesName+embassysEmployeesText+emigrant+emigrantText)
+
+    embassysEmployeesText = ": That`s sounds good. this even exceeds requirements, 5 years is enough.Do you have deposit in your card?\n"
+    emigrantText = ": Yes, but it`s about 5000\$.\n"
+
+        print(embassysEmployeesName+embassysEmployeesText+emigrant+emigrantText)
+
+    embassysEmployeesText = ": It must not be less than 15.000\$.Do you have friends and relatives in our country?\n"
+    emigrantText = ": No friends, but many relatives.\n"
+
+        print(embassysEmployeesName+embassysEmployeesText+emigrant+emigrantText)
+
+    embassysEmployeesText = ": That`s partially meets requirements. Ok, sir last question. How long are you going to stay in our country?\n"
+    emigrantText = ": I think 5 years is enough.\n"
+
+        print(embassysEmployeesName+embassysEmployeesText+emigrant+emigrantText)
+
+    embassysEmployeesText = ": Sir, it must be less than 3 years. So we can finish. thanks anyway\n"
+
+        print(embassysEmployeesName+embassysEmployeesText)
+}
